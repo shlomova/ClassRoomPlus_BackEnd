a22792a50a3d4603dd0440069faad8be9383c261
rafimahr <rafimahr3@gmail.com>
2024-04-30 12:11:40 +0300

    changed the name of the file

---
+++ controllers/usersControllers.js
@@ -0,0 +1,16 @@
+const asyncHandler = require('express-async-handler')
+const AppError = require('../utils/AppError')
+const User = require('./../models/usersModel')
+
+
+
+
+exports.getAllUsers = asyncHandler(async (req, res, next)=>{
+   
+    const users = await User.find()
+
+    res.status(200).json({
+        status:'success',
+        users
+})
+})
