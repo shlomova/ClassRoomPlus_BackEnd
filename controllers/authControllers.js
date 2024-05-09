becf27f83484b6b58b93c014de1f8003d0d84dfb
shlomova <79264751+shlomova@users.noreply.github.com>
2024-05-09 15:24:45 +0300

    add protect to restrictTo

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -5,78 +5,76 @@ const {promisify} = require('util')
 const jwt = require('jsonwebtoken')
 
 const signToken = id => {
-  return jwt.sign({ id, iat: Date.now() }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
-  });
+    return jwt.sign({id, iat: Date.now()}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
 };
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
     const cookieOptions = {
-      expires: new Date(
-        Date.now() + process.env.JWT_EXPIRES_IN * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true,
-      secure : true
+        expires: new Date(
+            Date.now() + process.env.JWT_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true,
+        secure: true
     }
-    
+
     res.cookie('jwt', token, cookieOptions);
     res.status(statusCode).json({
-      status: 'success',
-      token,
-      data: {
-        user
-      }
+        status: 'success',
+        token,
+        data: {
+            user
+        }
     });
-  };
-  
-exports.register = asyncHandler(async(req, res, next)=>{
-  console.log('hello')
+};
 
-    const {email, password, confirmPassword, firstName, lastName,phone,role} = req.body
-    if (!email ||!password ||!confirmPassword || !firstName || !lastName || !phone || !role) return next(new AppError(403,'Request details are missing'))
-    const newUser = await User.create({email, password, confirmPassword, firstName, lastName,phone,role})
-   createSendToken(newUser, 201 , res)
+exports.register = asyncHandler(async (req, res, next) => {
+    const {email, password, confirmPassword, firstName, lastName, phone, role} = req.body
+    if (!email || !password || !confirmPassword || !firstName || !lastName || !phone || !role) return next(new AppError(403, 'Request details are missing'))
+    const newUser = await User.create({email, password, confirmPassword, firstName, lastName, phone, role})
+    createSendToken(newUser, 201, res)
 })
 
-exports.login = asyncHandler(async (req, res, next)=>{
+exports.login = asyncHandler(async (req, res, next) => {
     const {email, password} = req.body
-    if(!email || !password) return next(new AppError(403, 'Email or password is missing'))
+    if (!email || !password) return next(new AppError(403, 'Email or password is missing'))
     const user = await User.findOne({email}).select('+password')
-if (! user || !await  user.checkPassword(password, user.password) ) return next(new AppError(403, 'Email or password is not correct 1'))
-   
-/* createSendToken(user, 201 , res) */
- 
+    if (!user || !await user.checkPassword(password, user.password)) return next(new AppError(403, 'Email or password is not correct 1'))
+
+    createSendToken(user, 201 , res)
+
 })
-exports.protect = asyncHandler(async(req,res, next)=>{
+const protect = asyncHandler(async (req, res, next) => {
     const token = req.headers.cookie.split('=')[1]
-    if(!token) return next(new AppError(403, 'Please login '))
+    if (!token) return next(new AppError(403, 'Please login '))
     const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
-    if(!decoded) return next(new AppError(403, 'Please login '))
+    if (!decoded) return next(new AppError(403, 'Please login '))
     const {id} = decoded
     const user = await User.findById(id)
-   if(!user) return next(new AppError(400, 'Please register'))
-     req.user = user
+    if (!user) return next(new AppError(400, 'Please register'))
+    req.user = user
     next()
 })
 
-exports.restrictTo = (...roles) => {
-    return (req, res, next) => {
-      
-      if (!roles.includes(req.user.role)) {
-        return next(
-          new AppError(403, 'You do not have permission to perform this action')
-        );
-      }
-  
-  
-      next();
+exports.restrictTo = (role) => {
+    return async (req, res, next) => {
+        await protect(req, res, next);
+        if (role !== req.user.role) {
+            return next(
+                new AppError(403, 'You do not have permission to perform this action')
+            );
+        }
     };
-  };
-  
-  exports.isByUser = asyncHandler(async(req, res, next)=>{
+};
+
+
+exports.isByUser = asyncHandler(async (req, res, next) => {
     const {userId} = req.params
     const user = req.user
-    if(userId !== user._id) return next(new AppError(403, 'You are not authorized to perform this action'))
+    if (userId !== user._id) return next(new AppError(403, 'You are not authorized to perform this action'))
     next()
-  })
+})
+
+exports.protect = protect;
