ce46c1734ecc71907d3c3ffad1fc8187a5440f61
rafimahr <rafimahr3@gmail.com>
2024-06-02 20:17:53 +0300

    refactor: Remove admin restriction for getting all users

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const usersControllers = require('./../controllers/usersControllers')
 const router = express.Router()
 
 
-router.route('/').get(authControllers.protect, authControllers.restrictTo('admin'), usersControllers.getAllUsers)
+router.route('/').get(authControllers.protect, usersControllers.getAllUsers)
 
 router.route('/login').post(authControllers.login)
 router.route('/signup').post(authControllers.register)
