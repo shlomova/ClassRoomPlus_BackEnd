54efd94891fa34ad5deb64a62cb814a9f9599c28
rafimahr <rafimahr3@gmail.com>
2024-06-02 20:16:15 +0300

    chore: Add mail router to app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const fileRouter = require('./routes/filerouts')
 const usersRouter = require('./routes/users');
 const postRouter = require('./routes/post');
 const courseRouter = require('./routes/course');
-
+const mailRouter = require('./routes/mail')
 const app = express();
 
 app.use(cors({
@@ -19,13 +19,13 @@ app.use(express.urlencoded({ extended: true }));
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'pug');
-
 // app.use(logger('dev'));
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/mail', mailRouter)
 app.use('/users', usersRouter);
 app.use('/posts', postRouter);
 app.use('/courses', courseRouter);
